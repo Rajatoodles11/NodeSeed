2b7ffef8d79d13189033ec2dd54f9b187d97f98b
Rajat khurana <rajat.khurana@oodlestechnologies.com>
2017-09-24 12:29:03 +0530

    Updated

--- README.md
+++
@@ -1,2 +0,0 @@
-# NodeSeed
-Learning Node from Scratch and develop seed
--- configrations/config.js
+++
@@ -1,7 +0,0 @@
-
-// load the config file for the current environment
-global.config = require('./env/' + process.env.NODE_ENV);
-
-// console.log("config object is:",config);
-// export config
-module.exports = config;
